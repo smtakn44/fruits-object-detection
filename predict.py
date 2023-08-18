f5d3084d4473dbbe7e9261a9c14a74af1be1494f
Samet Akın <96833901+smtakn44@users.noreply.github.com>
2023-08-18 09:32:48 +0300

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,3 +1,5 @@
 from ultralytics import YOLO
-model=YOLO("best.pt")
-model.predict(source='image.jpg', show=True, save=True, hide_labels=False, hide_conf=False, conf=0.5, save_txt=False, save_crop=False, line_thickness=2)
+
+model = YOLO("best.pt")
+
+model.predict(source='indir.jpg', show=True, save=True, hide_labels=False, hide_conf=False, conf=0.5, save_txt=True, save_crop=True, line_thickness=2)
